9ef24a91143cc52f18c535e89f73b47786e3e8bb
zewebdev1337 <112774130+zewebdev1337@users.noreply.github.com>
2024-11-13 12:45:45 -0600

    update deployment paths

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-systemctl disable --user --now prices.service
+systemctl disable --user --now brypto_price_tracker.service
 
 if [ ! -d "build" ]; then
     mkdir build
@@ -14,18 +14,18 @@ fi
 cd build
 qmake ../widget.pro
 make
-mv price-tracker /usr/local/bin/brypto-price-tracker
+mv price-tracker /usr/local/bin/brypto_price_tracker
 cd ..
 rm -rf build
 
-cat << 'EOF' > ~/.config/systemd/user/prices.service
+cat << 'EOF' > ~/.config/systemd/user/brypto_price_tracker.service
 [Unit]
 Description=Brypto price tracker service. It starts after successfully connecting to the internet
 After=network.target
 Wants=network-online.target
 
 [Service]
-ExecStart=/usr/local/bin/brypto-price-tracker
+ExecStart=/usr/local/bin/brypto_price_tracker
 WorkingDirectory=/usr/local/bin/
 Restart=always
 RestartSec=30
@@ -34,5 +34,5 @@ RestartSec=30
 WantedBy=default.target
 EOF
 
-systemctl enable --user --now prices.service
+systemctl enable --user --now brypto_price_tracker.service
 
